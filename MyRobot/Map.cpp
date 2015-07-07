f0c9da11aac79ed402555201f11adef41114d08d
sergeyalh <SerG1206>
2015-07-07 06:46:51 -0700

    new mp

--- MyRobot/Map.cpp
+++ MyRobot/Map.cpp
@@ -7,49 +7,43 @@
 #include "lodepng.h"
 
 std::vector<unsigned char> image; //the raw pixels
+std::vector<unsigned char> newImage; //the raw pixels
 unsigned width, height, resolution;
-int** map;
+
+bool doIt = 1;
 
 //Default C'Tor
 Map::Map(){
-	ConfigurationManager *configFile = ConfigurationManager::getInstance();
-	string mapPath = configFile->getMap();
-	resolution = configFile->getGridResolutionCM();
-	loadImage(mapPath);
+	if(doIt == 1){
+		doIt = 0;
+		ConfigurationManager *configFile = ConfigurationManager::getInstance();
+		string mapPath = configFile->getMap();
+		resolution = configFile->getGridResolutionCM();
+		loadImage(mapPath);
 
-	int count = 0;
-	//Initialize the map
-	map = new int*[height];
-	for (int i=0; i<height;i++){
-		map[i] = new int[width];
-		for (int j=0; j<width;j++){
-			// Check if the pixel is all white
-			if((image[count]==255)&&(image[count+1]==255)&&(image[count+2]==255)&&(image[count+3]==255)){
-				map[i][j] = FREE;
-			// If not all white then OBSTACLE
-			} else {
-				map[i][j] = OBSTACLE;
-/*				if(i>0  && j>0)
-					try{map[i-1][j-1] = OBSTACLE;}catch (exception& e){ cout << e.what() << '\n';}
-				if(j>0)
-					try{map[i][j-1] = OBSTACLE;}catch (exception& e){ cout << e.what() << '\n';}
-				if(i<height && j>0)
-					try{map[i+1][j-1] = OBSTACLE;}catch (exception& e){ cout << e.what() << '\n';}
-				if(i>0)
-					try{map[i-1][j] = OBSTACLE;}catch (exception& e){ cout << e.what() << '\n';}
-				if(i<height)
-					try{map[i+1][j] = OBSTACLE;}catch (exception& e){ cout << e.what() << '\n';}
-				if(i>0 && j<width)
-					try{map[i-1][j+1] = OBSTACLE;}catch (exception& e){ cout << e.what() << '\n';}
-				if(j<width)
-					try{map[i][j+1] = OBSTACLE;}catch (exception& e){ cout << e.what() << '\n';}
-				if(i<height && j<width)
-					try{map[i+1][j+1] = OBSTACLE;}catch (exception& e){ cout << e.what() << '\n';}*/
+		newImage.resize(width * height * 4);
+		unsigned int y,x,i,j;
 
+		for (y = 0; y < newImage.size(); y++)
+			newImage[y]=255;
+
+		for (y = 0; y < height; y++)
+			for (x = 0; x < width; x++) {
+				if (!(image[y * width * 4 + x * 4 + 0]
+							|| image[y * width * 4 + x * 4 + 1]
+							|| image[y * width * 4 + x * 4 + 2])){
+					for (i = y - configFile->getRobotSizeY()/10; i <= y + configFile->getRobotSizeY()/10; i++){
+						for (j = x - configFile->getRobotSizeX()/10; j <= x + configFile->getRobotSizeX()/10; j++){
+							if ((i>=0)&&(j>=0)&&(i<height)&&(j<width)){
+									newImage[i * width * 4 + j * 4 + 0] = 0;
+									newImage[i * width * 4 + j * 4 + 1] = 0;
+									newImage[i * width * 4 + j * 4 + 2] = 0;
+							}
+						}
+					}
+				}
 			}
-			// + 4 because we work with RBGA for each pixel
-			count = count + 4;
-		}
+		lodepng::encode("newFat.png", newImage, width, height);
 	}
 }
 
@@ -76,7 +70,13 @@ void Map::getMapCellByPosition(float xPosition,float yPosition,int &x,int &y){
 }
 
 //A method which returns a map cell's value by a given X & Y coordinates
-int Map::getMapCellValue(int xPosition,int yPosition){return map[xPosition][yPosition];}
+int Map::getMapCellValue(int xPosition,int yPosition){
+	if((newImage[yPosition * width * 4 + xPosition * 4 + 0] == 0)&&
+			(newImage[yPosition * width * 4 + xPosition * 4 + 1] = 0)&&
+			(newImage[yPosition * width * 4 + xPosition * 4 + 2] = 0))
+		return FREE;
+	return OBSTACLE;
+}
 
 //This method load the pic to map[][]
 void Map::loadImage(string filename){
@@ -86,44 +86,8 @@ void Map::loadImage(string filename){
 	if(error) std::cout << "decoder error " << error << ": " << lodepng_error_text(error) << std::endl;
 }
 
-//This methode save the vector back to the pic with the path
-void Map::saveImage(string filename,Node notesarr[]){
-	std::vector<unsigned char> newImage = image;
-	int size = (sizeof(notesarr)/sizeof(*notesarr));
-	//  color the path
-	for (unsigned int i = 0; size; i++){
-		Node curNode = notesarr[i];
-		int statBitPlace = (width*(curNode.getyPos()-1)*4)+(curNode.getxPos()*4)-4;
-		newImage[statBitPlace] = 8;
-		newImage[statBitPlace + 1] = 255;
-		newImage[statBitPlace + 2] = 2;
-		newImage[statBitPlace + 3] = 4;
-	}
-
-	//Encode the image
-	unsigned error = lodepng::encode(filename, newImage, width, height);
-	//if there's an error, display it
-	if(error) std::cout << "encoder error " << error << ": "<< lodepng_error_text(error) << std::endl;
-}
-
-//A methode which save back the map with path
-void Map::saveMapWithPathToPic(Node notesarr[]){
-	// Get list of dots.
-	string newMapPath = "mapWithPath.png";
-	saveImage(newMapPath,notesarr);
-}
-
 //return height
 int Map::getHeight(){return height;}
 
 //return width
 int Map::getWidth(){return width;}
-
-//A method which prints a particle's map
-void Map::printParticleMap(){
-    for (int i=0; i<height; i++){
-    	for (int j=0; j<width; j++)
-    		cout<<map[i][j];
-    	cout<<endl;
-    }
-}
