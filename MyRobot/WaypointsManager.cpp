38a1da814e9819d38caf6248ee4139e5739e1cc1
akselrodavi‏ <akselrodavi@gmail.com>
2015-07-21 04:19:06 -0700

    asd

--- MyRobot/WaypointsManager.cpp
+++ MyRobot/WaypointsManager.cpp
@@ -16,20 +16,74 @@ WaypointsManager::WaypointsManager() {
 
 vector<Node*> WaypointsManager::generateWayPoints(Node * startPoint,Node * endPoint)
 {
+	//        90
+	//    135    45
+	// 180          0
+	//    225    315
+	//       270
+
 	Astar * star = new Astar();
 	vector<Node*> path = star->PathPlanner(startPoint,endPoint);
 	int pathSize = path.size();
 	vector<Node*> resultWaypoints;
 
-
-	for (int i=1; i < pathSize - 1; i++)
+	for (int index=0; index < pathSize - 1; index++)
 	{
-		// set waypoints here
+		int engle = 0;
+		int pointX1 = resultWaypoints[index]->getxPos();
+		int pointY1 = resultWaypoints[index]->getyPos();
+		int pointX2 = resultWaypoints[index+1]->getxPos();
+		int pointY2 = resultWaypoints[index+1]->getyPos();
+
+		// Check if they have the same horizontal
+		if (pointX1== pointX2)
+		{
+			// Check if up
+			if(pointY2 > pointY1)
+			{
+				engle = 90;
+			}
+			else
+			{
+				engle = 270;
+			}
+		}
+		// Check if the point is righter
+		if (pointX2 > pointX1)
+		{
+			// Check if upper
+			if (pointY2 > pointY1)
+			{
+				engle = 45;
+			}
+			else
+			{
+				engle = 315;
+			}
+		}
+		// Check if lefter
+		if (pointX2 < pointX1)
+		{
+			// Check if upper
+			if (pointY2 < pointY1)
+			{
+				engle = 135;
+			}
+			else
+			{
+				engle = 225;
+			}
+		}
+
+		// Behavior move to engle until point 2
+
+
 	}
 	return resultWaypoints;
 }
 
-WaypointsManager::~WaypointsManager() {
+WaypointsManager::~WaypointsManager()
+{
 	// TODO Auto-generated destructor stub
 }
 
